5b20a8e5d0af97ab1d81f2896918604359bd36b2
Rinat Khusainov <khusainovrm@yandex.ru>
2023-05-05 22:14:43 +0300

    build data

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { Quasar } from 'quasar'
 import quasarLang from 'quasar/lang/ru'
-// import { registerSW } from 'virtual:pwa-register'
-//
-// const updateSW = registerSW({
-//   onOfflineReady() {}
-// })
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onOfflineReady() {}
+})
 
 // Import Quasar css
 import 'quasar/dist/quasar.css'
