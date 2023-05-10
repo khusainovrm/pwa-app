9703cf3c18d575d672fa4c46851c34a0fce53483
Rinat Khusainov <khusainovrm@yandex.ru>
2023-05-10 08:36:39 +0300

    mut response

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,6 @@
 import queryString from 'query-string'
-import axios, { AxiosError } from 'axios'
+import axios from 'axios'
 import type { AxiosResponse } from 'axios'
-import { useQuasar } from 'quasar'
 
 const DEFAULT_ERROR_TEXT = 'Произошла ошибка...'
 
@@ -29,7 +28,6 @@ export const formatRes = (res: AxiosResponse) => Promise.resolve(res.data)
 
 export const formatErr = (err: any, options: { prefix?: string } = {}) => {
   const isPrefix = !!options.prefix
-  const $q = useQuasar()
 
   let error: any = new Error()
   if (err.response && err.response.data && err.response.data.error) {
@@ -44,13 +42,7 @@ export const formatErr = (err: any, options: { prefix?: string } = {}) => {
     error = err.response
   } else if (!err.response && err.message === 'Network Error' && !err.status) {
     error = err
-    if ($q.platform.is.mobile) {
-      error.description = isPrefix ? options.prefix + `: ${DEFAULT_ERROR_TEXT}` : DEFAULT_ERROR_TEXT
-    } else {
-      error.description = isPrefix
-        ? options.prefix + ': нет соединения с интернетом!'
-        : 'Нет соединения с интернетом!'
-    }
+    error.description = isPrefix ? options.prefix + ': ошибка сети!' : 'Ошибка сети!'
   }
   if (err.response !== undefined && err.response.status) {
     error.status = err.response.status
--- src/my-worker.ts
+++ src/my-worker.ts
@@ -22,25 +22,20 @@ self.onmessage = async (e) => {
               const allTaskCache = await caches.open(name)
               console.log('taskCache', allTaskCache)
 
-              const response = allTaskCache.match(
+              const originalResponse = await allTaskCache.match(
                 new URL('https://crudcrud.com/api/b437cb27126c4130804c53ed41af685e/task/')
               )
-              console.log('response', response)
-              const jsonResponse = new Response(e.data.taskArguments, {
-                headers: {
-                  'Content-Type': 'application/json'
-                }
-              })
-              console.log('jsonResponse', jsonResponse)
+              console.log('response', originalResponse)
+              console.log('e.data.taskArguments', e.data.taskArguments)
+              const newResponse = new Response(e.data.taskArguments, originalResponse)
+              console.log('newResponse', newResponse)
 
               const tasksCache = await allTaskCache.keys()
               tasksCache.map(async (request) => {
                 if (
                   request.url === 'https://crudcrud.com/api/b437cb27126c4130804c53ed41af685e/task/'
                 ) {
-                  console.log('request before', await request)
-                  await allTaskCache.put(request.clone(), jsonResponse)
-                  console.log('request after', await allTaskCache.keys().then(console.log))
+                  await allTaskCache.put(request, newResponse)
                 } else {
                   await allTaskCache.delete(request)
                 }
