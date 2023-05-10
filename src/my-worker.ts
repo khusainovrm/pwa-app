68c1e82ed0b00e5b6428894efd7e4ed9da74e745
Rinat Khusainov <khusainovrm@yandex.ru>
2023-05-10 10:13:36 +0300

    0e797caa1ef849b08bb5f493673b4e88

--- src/my-worker.ts
+++ src/my-worker.ts
@@ -1,6 +1,6 @@
 import { msg } from './workerImport'
 const tasksCacheName = 'api-task'
-const URL_TO_HANDLE = 'https://crudcrud.com/api/b437cb27126c4130804c53ed41af685e/task/'
+const URL_TO_HANDLE = 'https://crudcrud.com/api/0e797caa1ef849b08bb5f493673b4e88/task/'
 
 let counter = 1
 
